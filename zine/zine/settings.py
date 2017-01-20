2b6bb450190ed22c1f38a0a1453867dfbeee6812
Gabrielle Harrison <gabrielle@codesters.com>
2017-01-19 22:02:44 -0500

    edited wsgi

--- zine/zine/settings.py
+++ zine/zine/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'zine.wsgi.journal'
+WSGI_APPLICATION = 'zine.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
