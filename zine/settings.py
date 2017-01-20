78d8505a89fbcb613c6db5df40cdb575584d50af
Gabrielle Harrison <gabrielle@codesters.com>
2017-01-19 22:49:13 -0500

    changed settings file path

--- zine/settings.py
+++ zine/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
