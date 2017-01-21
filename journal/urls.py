b7e5ff0b21337bd2d15e21dd539e8e9a06183f30
Gabrielle Harrison <gabrielle@codesters.com>
2017-01-20 20:03:06 -0500

    added url thing

--- journal/urls.py
+++ journal/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
 ]
 
-# if not settings.DEBUG:
-#     urlpatterns += patterns('',
-#         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-#     )
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
