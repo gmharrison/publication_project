af633371cefc7d41a40e67ce01f4cc3c938537f2
Gabrielle Harrison <gabrielle@codesters.com>
2017-01-20 20:17:42 -0500

    reset urls

--- journal/urls.py
+++ journal/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls import patterns
 
 from . import views
-
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
 ]
 
-urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-)
+# if not settings.DEBUG:
+#     urlpatterns += patterns('',
+#         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+#     )
