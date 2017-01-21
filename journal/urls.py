b2744b1390b3f74fe1538f03835ed1ddbd098757
Gabrielle Harrison <gabrielle@codesters.com>
2017-01-20 20:13:12 -0500

    plz

--- journal/urls.py
+++ journal/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import patterns
 
 from . import views
+
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
