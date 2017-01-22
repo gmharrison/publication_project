b66abcd1482c1aac725d650ff8c2392bdfa107de
Gabrielle Harrison <gabrielle@codesters.com>
2017-01-22 15:04:22 -0500

    changed urls file

--- journal/urls.py
+++ journal/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from django.conf import settings
+from zine import settings
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
