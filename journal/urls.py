5bc168d3b7ef2e1167587d300dd9a0c7fe0e7024
Gabrielle Harrison <gabrielle@codesters.com>
2017-01-22 18:06:20 -0500

    maybe this will work lol?

--- journal/urls.py
+++ journal/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
-from zine import settings
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
